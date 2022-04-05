0cc33f0566c143792c5a922a352a96524bbe3725
Dhananjay <dhananjaysingh9870ds@gmail.com>
2022-04-05 11:34:21 +0530

    Initial commit

---
+++ src/component/Basics/Resturant.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./style.css";
+import Menu from "../menuApi";
+import MenuCard from "./menuCard";
+import Navbar from "./navbar";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+console.log(uniqueList);
+
+const Resturant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
+    </>
+  );
+};
+
+export default Resturant;
+
+// const uniqueList = [
+//   ...new Set(
+//     Menu.map((curElem) => {
+//       return curElem.category;
+//     })
+//   ),
+// ];
+
+// const Resturant = () => {
+//   const [menuData, setMenuData] = useState(Menu);
+//   const [menuList, setMenuList] = useState(uniqueList);
+
+//   const filterItem = (category) => {
+//     const updatedList = Menu.filter((curElem) => {
+//       return curElem.category === category;
+//     });
+//     setMenuData(updatedList);
+//   };
+
+//   return (
+//     <>
+//       <Navbar filterItem={filterItem} />
+//       <MenuCard menuData={menuData} menuList={menuList} />;
+//     </>
+//   );
+// };
+
+// export default Resturant;
